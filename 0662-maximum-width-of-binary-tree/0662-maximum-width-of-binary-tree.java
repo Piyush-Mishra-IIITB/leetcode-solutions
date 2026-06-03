fd5c4a3b450666eef42bb47072fce7b42fc5ab7e
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-06-03 10:18:55 +0530

    Time: 1 ms (99.42%), Space: 45 MB (63.28%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -1,18 +1,5 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+ 
 class Solution {
     public class pair{
         TreeNode root;
@@ -21,38 +8,36 @@ class Solution {
             this.root=root;
             this.index=index;
         }
-
     }
-    public int widthOfBinaryTree(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        int wid=0;
+    int max=0;
+    public int widthOfBinaryTree(TreeNode root) {  
         Queue<pair>qq=new LinkedList<>();
         qq.add(new pair(root,0));
+        
         while(!qq.isEmpty()){
             int size=qq.size();
-            int min=qq.peek().index;
             int first=0;
             int last=0;
             for(int i=0;i<size;i++){
                 pair p=qq.poll();
                 TreeNode curr=p.root;
-                int ind=p.index-min;
-                if(i==0){
-                    first=ind;
-                }if(i==size-1){
-                    last=ind;
-                }
-                if(curr.left!=null){
-                    qq.add(new pair(curr.left,2*ind+1));
-                }
-                if(curr.right!=null){
-                    qq.add(new pair(curr.right,2*ind+2));
-                }
-                wid=Math.max(last-first+1,wid);
+                int ind=p.index;  
+               if(i==0){
+                first=p.index;
+               }
+               if(i==size-1){
+                last=p.index;
+               }
+              if(curr.left!=null){
+                qq.add(new pair(curr.left,2*ind+1));
+              }
+              if(curr.right!=null){
+                qq.add(new pair(curr.right,2*ind+2));
+              }
+              
             }
+             max=Math.max(max,last-first+1);
         }
-        return wid;
+        return max;  
     }
 }
