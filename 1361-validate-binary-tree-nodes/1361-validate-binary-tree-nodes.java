acda7c6f774a30e2fe59da768a5168db1b89a9c1
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-06-17 13:39:30 +0530

    Time: 12 ms (25%), Space: 48.6 MB (17.28%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
@@ -0,0 +1,70 @@
+class Solution {
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        List<List<Integer>>ll=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            ll.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            int a=leftChild[i];
+            int b=rightChild[i];
+            if(a!=-1 ){
+                ll.get(i).add(a);
+            }
+            if(b!=-1 ){
+                ll.get(i).add(b);
+            }
+        }
+
+       HashMap<Integer,Integer>hm=new HashMap<>();
+       int indegree[]=new int[n];
+       for(int i=0;i<n;i++){
+            int a=leftChild[i];
+            int b=rightChild[i];
+            if(a!=-1){
+                indegree[a]++;
+            }
+            if(b!=-1){
+                indegree[b]++;
+            }
+       }
+       int count=0;
+       int start=-1;
+       for(int i=0;i<indegree.length;i++){
+           if(indegree[i]==0){
+            start=i;
+            count++;
+           }
+       }
+       if(count!=1){
+        return false;
+       }
+       if(start==-1){
+        return false;
+       }
+        boolean vis2[]=new boolean[n];
+        if(!dfs(start,vis2,ll)){
+            return false;
+        }else{
+            for(int i=0;i<vis2.length;i++){
+                if(vis2[i]==false){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+    public boolean dfs(int i,boolean vis[],List<List<Integer>>ll){
+        vis[i]=true;
+        for(Integer it:ll.get(i)){
+            if(vis[it]==true){
+               return false;
+              }
+                if(!dfs(it,vis,ll)){
+                    return false;
+                }
+            }
+             return true;
+        }  
+    }
+     
