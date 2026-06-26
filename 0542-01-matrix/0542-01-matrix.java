a4c7d919cf043102c30ea2384be8177f590e0606
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-06-26 19:27:51 +0530

    Time: 15 ms (41.63%), Space: 48.6 MB (90.87%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -13,43 +13,52 @@ class Solution {
         Queue<pair>qq=new LinkedList<>();
         int n=arr.length;
         int m=arr[0].length;
-        int d[][]=new int[n][m];
+        int dist[][]=new int [n][m];
         boolean vis[][]=new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(arr[i][j]==0){
                     qq.add(new pair(i,j,0));
-                    d[i][j]=0;
                     vis[i][j]=true;
                 }
             }
         }
         while(!qq.isEmpty()){
-            pair curr=qq.poll();
-            int row=curr.row;
-            int col=curr.col;
-            int dist=curr.dis;
-            if(row+1<n && vis[row+1][col]==false){
-                vis[row+1][col]=true;
-                qq.add(new pair(row+1,col,dist+1));
-                d[row+1][col]=dist+1;
+            pair p=qq.poll();
+            int r=p.row;
+            int c=p.col;
+            int d=p.dis;
+            if(r+1<n){
+                if(vis[r+1][c]==false){
+                    vis[r+1][c]=true;
+                    dist[r+1][c]=d+1;
+                    qq.add(new pair(r+1,c,d+1));
+                }
             }
-            if(row-1>=0 && vis[row-1][col]==false){
-                vis[row-1][col]=true;
-                qq.add(new pair(row-1,col,dist+1));
-                d[row-1][col]=dist+1;
+            if(r-1>=0){
+                if(vis[r-1][c]==false){
+                    vis[r-1][c]=true;
+                    dist[r-1][c]=d+1;
+                    qq.add(new pair(r-1,c,d+1));
+                }
             }
-            if(col+1<m && vis[row][col+1]==false){
-                vis[row][col+1]=true;
-                qq.add(new pair(row,col+1,dist+1));
-                d[row][col+1]=dist+1;
-            }if(col-1>=0 && vis[row][col-1]==false){
-                vis[row][col-1]=true;
-                qq.add(new pair(row,col-1,dist+1));
-                d[row][col-1]=dist+1;
+            if(c+1<m){
+                if(vis[r][c+1]==false){
+                    vis[r][c+1]=true;
+                    dist[r][c+1]=d+1;
+                    qq.add(new pair(r,c+1,d+1));
+                }
             }
+            if(c-1>=0){
+                if(vis[r][c-1]==false){
+                    vis[r][c-1]=true;
+                    dist[r][c-1]=d+1;
+                    qq.add(new pair(r,c-1,d+1));
+                }
+            }
+
         }
-        return d;
+        return dist;
     }
     
 }
