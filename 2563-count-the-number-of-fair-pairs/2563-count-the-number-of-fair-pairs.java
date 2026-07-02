27c016159b831964d59b5fbff797c353a01e2c46
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-07-02 20:31:39 +0530

    Time: 82 ms (31.74%), Space: 85.8 MB (67.17%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -1,29 +1,34 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
-
         Arrays.sort(nums);
-        long count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            int left = lowerBound(nums, i + 1, nums.length, lower - nums[i]);
-            int right = lowerBound(nums, i + 1, nums.length, upper - nums[i]+1 );
-
-            count += (right - left);
-        }
-        return count;
-    }
-    public int lowerBound(int[] nums, int start, int end, int target) {
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] >= target) {
-                end = mid;
-            } else {
-                start = mid + 1;
+       int n=nums.length;
+       long count=0;
+       for(int i=0;i<nums.length;i++){
+        int lowe=helper(i+1,n-1,nums,lower-nums[i]);
+        int higher=helper2(i+1,n-1,nums,upper-nums[i]);
+        count+=higher-lowe;
+       }
+       return count;}
+       public int helper(int start,int end,int arr[],int target){
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>=target){
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
         }
-
         return start;
-    }
+       }
+       public int helper2(int start,int end,int arr[],int target){
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]<=target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return start;
+       }
 }
