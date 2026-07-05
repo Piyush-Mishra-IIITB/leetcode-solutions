71711c8d0e464e2f5a63a775440a472519d31f9c
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-07-05 14:33:55 +0530

    Time: 8 ms (74.46%), Space: 65.6 MB (13.65%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -3,22 +3,20 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>>ans=new ArrayList<>();
         List<String>ll=new ArrayList<>();
-    
-        helper(0,ll,ans,s);
+        helper(ans,ll,s,0);
         return ans;
     }
-    public void helper(int index,List<String>ll,List<List<String>>ans,String s){
-       if(index==s.length()){
-            ans.add(new ArrayList<>(ll));
-            return;
+    public void helper(List<List<String>>ans,List<String>ll,String s,int ind){
+       if(ind==s.length()){
+        ans.add(new ArrayList<>(ll));
        }
-       for(int i=index;i<s.length();i++){
-           if(palind(s,index,i)){
-            ll.add(s.substring(index,i+1));
-            helper(i+1,ll,ans,s);
-            ll.remove(ll.size()-1);
-           }
-       }
+        for(int i=ind;i<s.length();i++){
+            if(palind(s,ind,i)){
+                ll.add(s.substring(ind,i+1));
+                helper(ans,ll,s,i+1);
+                ll.remove(ll.size()-1);
+            }
+        }
     }
     public boolean palind(String s,int i,int j){
         while(i<j){
