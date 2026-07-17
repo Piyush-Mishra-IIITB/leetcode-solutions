e7494bef82ee9014bdd3978a340e120ac720b4b3
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-07-17 09:50:40 +0530

    Time: 30 ms (28.58%), Space: 51 MB (53.81%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,27 +1,25 @@
 class Solution {
     public int smallestDivisor(int[] arr, int threshold) {
-        int end=arr[0];
-        for(int i=0;i<arr.length;i++){
-            end=Math.max(arr[i],end);
-        }
-        int start=1;
-        int output=end;
-        while(start<=end){
-           int mid=start+(end-start)/2;
-           if(helper(arr,mid)<=threshold){
-              output=Math.min(output,mid);
-              end=mid-1;
-           }else{
+       int start=1;
+       int end=-1;
+       for(int i=0;i<arr.length;i++){
+        end=Math.max(end,arr[i]);
+       }
+       while(start<=end){
+        int mid=start+(end-start)/2;
+         if(helper(mid,arr,threshold)){
+            end=mid-1;
+         }else{
             start=mid+1;
-           }
-        }
-        return output;
-    } 
-    public int helper(int arr[],int k){
-        int counter=0;
+         }
+       }
+       return start;
+    }
+    public boolean helper(int divisor,int arr[],int threshold){
+        int sum=0;
         for(int i=0;i<arr.length;i++){
-            counter+=Math.ceil(arr[i]/(double)k);
+           sum += (int) Math.ceil(arr[i] * 1.0 / divisor);
         }
-        return counter;
+        return sum<=threshold;
     }
 }
