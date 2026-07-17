20ab6bed4c816dfa718862e26bf8bc3717f41b5e
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-07-17 09:33:11 +0530

    Time: 1657 ms (5.77%), Space: 84.6 MB (12.64%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,51 +1,68 @@
 class Solution {
     public int minDays(int[] arr, int m, int k) {
-        if((long)m*k>arr.length){
+        if ((long) m * k > arr.length) {
             return -1;
         }
-        int output=Integer.MAX_VALUE;
-        int start=arr[0];
-        int end=arr[0];
-        for(int i=0;i<arr.length;i++){
-            start=Math.min(start,arr[i]);
-            end=Math.max(end,arr[i]);
-     }
-     while(start<=end){
-               int mid=start+(end-start)/2;
-               if(helper(arr,m,k,mid)>=m){
-                     output=Math.min(output,mid);
-                     end=mid-1;
-                }
-                else{
-                    start=mid+1;
-                }        
-     }
-                
-        
-        return output;
-    }
-    public int helper(int arr[],int m,int k,int i){
-        
-        boolean vis[]=new boolean[arr.length];
-        for(int j=0;j<arr.length;j++){
-            if(arr[j]<=i){
-                vis[j]=true;
+
+        int max = -1;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i], max);
+            min = Math.min(min, arr[i]);
+        }
+
+        int start = min;
+        int end = max;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (helper(mid, arr, m, k)) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-        int counter=0;
-        int a=0;
-        for(int j=0;j<vis.length;j++){
-             if(vis[j]==true){
-                a++;
-             }
-             if(a==k){
-                counter++;
-                a=0;
-             }
-             if(vis[j]==false){
-                a=0;
-             }
+
+        return start;
+    }
+
+    public boolean helper(int days, int[] arr, int m, int k) {
+        boolean vis[] = new boolean[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= days) {
+                vis[i] = true;
+            } else {
+                vis[i] = false;
+            }
         }
-        return counter;
+
+        int count = 0;
+        int i = 0;
+        int j = k - 1;
+
+        while (j < arr.length) {
+            if (help(vis, i, j)) {
+                count++;
+                i = j + 1;
+                j = j + k;
+            } else {
+                i++;
+                j++;
+            }
+        }
+
+        return count >= m;
+    }
+
+    public boolean help(boolean[] vis, int i, int j) {
+        for (int k = i; k <= j; k++) {
+            if (!vis[k]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
