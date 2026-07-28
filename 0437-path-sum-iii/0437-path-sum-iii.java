3a8ce8a02d8c6d55ef576db92682a53697e8fbe2
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-07-28 18:34:14 +0530

    Time: 18 ms (39.46%), Space: 44.8 MB (90.24%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -1,36 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */class Solution {
-    int no=0;
+class Solution {
     public int pathSum(TreeNode root, int target) {
-   if(root==null ){
-       return 0;
-   }
-   helper(0,target,root);
-   pathSum(root.left,target);
-    pathSum(root.right,target);
-   return no;
+        if (root == null) {
+            return 0;
+        }
+
+        return helper(root, (long) target)
+             + pathSum(root.left, target)
+             + pathSum(root.right, target);
     }
-   public void helper(long sum,int target,TreeNode root){
-    if(root==null){
-        return;
+
+    public int helper(TreeNode root, long target) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ans = 0;
+
+        if (target == root.val) {
+            ans++;
+        }
+
+        ans += helper(root.left, target - root.val);
+        ans += helper(root.right, target - root.val);
+
+        return ans;
     }
-    if(root.val+sum==target){
-        no++;
-    }
-    helper(sum+root.val,target,root.left);
-    helper(sum+root.val,target,root.right);
-   }
 }
