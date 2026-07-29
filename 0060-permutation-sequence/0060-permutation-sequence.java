2d4ddda3f29746816a9d889b637b80077af3ad0e
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-07-29 08:15:50 +0530

    Time: 1 ms (99.69%), Space: 42.8 MB (76%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,35 +1,31 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        int arr[]=new int[n];
-        boolean vis[]=new boolean[n];  
-        for(int i=1;i<=n;i++){
-            arr[i-1]=i;
+        StringBuilder sb=new StringBuilder();
+        HashSet<Integer>hs=new HashSet<>();
+        for(int i=0;i<n;i++){
+           for(int j=1;j<=n;j++){
+             if(hs.contains(j)){
+                continue;
+             }
+             int w=ways(n-(i+1));
+             if(k>w){
+                k-=w;
+             }else{
+                sb.append(j);
+                hs.add(j);
+                break;
+             }
+           }
         }
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer>ll=new ArrayList<>();
-        helper(arr,k,vis,ll,ans);
-        Collections.sort(ans, (a,b) -> a.get(0) - b.get(0));
-        StringBuilder s=new StringBuilder("");
-        List<Integer> l=ans.get(k-1);
-        for(int i=0;i<l.size();i++){
-            s.append(l.get(i));
-        }
-        return s.toString();
+        return sb.toString();
     }
-    public void helper(int arr[],int k,boolean vis[],List<Integer>ll,List<List<Integer>>ans){
-      if(ll.size()==arr.length){
-        ans.add(new ArrayList<>(ll));
-        return;
-      }
-
-        for(int i=0;i<arr.length;i++){
-            if(vis[i]==false){
-                ll.add(arr[i]);
-                vis[i]=true;
-                helper(arr,k,vis,ll,ans);
-                ll.remove(ll.size()-1);
-                vis[i]=false;
-            }
+    public int ways(int n){
+          return fact(n);
+    }
+    public int fact(int n){
+        if(n==0 || n==1){
+            return 1;
         }
+        return n*fact(n-1);
     }
 }
