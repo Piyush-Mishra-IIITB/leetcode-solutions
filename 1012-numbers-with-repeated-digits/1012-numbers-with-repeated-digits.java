52596f9006f6b48936f52b15cebb3c244ebdd6a7
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-02 11:56:25 +0530

    Time: 78 ms (9.39%), Space: 47.8 MB (6.35%) - LeetHub

---
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.java
@@ -0,0 +1,51 @@
+class Solution {
+    Integer dp[][][][];
+    public int numDupDigitsAtMostN(int n) {
+        dp=new Integer[10][2][1025][2];
+        int x=n;
+        StringBuilder sb=new StringBuilder();
+        while(n>0){
+            sb.append(n%10);
+            n=n/10;
+        }
+        return x-(helper(0,1,0,1,sb.reverse().toString())-1);
+    }
+    public int helper(int ind,int tide,int mask,int lz,String s){
+        if(ind==s.length()){
+            return 1;
+        }
+        if(dp[ind][tide][mask][lz]!=null){
+            return dp[ind][tide][mask][lz];
+        }
+        int ans=0;
+        int lw=0;
+        int uw=9;
+        if(tide==1){
+            uw=s.charAt(ind)-'0';
+        }
+        for(int i=lw;i<=uw;i++){
+            if(lz==1 && i==0){
+                int newTide=tide;
+                    if(tide==1 && i == s.charAt(ind) - '0'){
+                        newTide=1;
+                    }else{
+                        newTide=0;
+                    }
+                ans+=helper(ind+1,newTide,mask,1,s);
+            }else{
+                if((mask & (1<<i))==0){
+                    int newMask=mask |(1<<i);
+                    int newTide=tide;
+                    if(tide==1 && i == s.charAt(ind) - '0'){
+                        newTide=1;
+                    }else{
+                        newTide=0;
+                    }
+                    ans+=helper(ind+1,newTide,newMask,0,s);
+                }
+            }
+            
+        }
+        return dp[ind][tide][mask][lz]=ans;  
+    }
+}
