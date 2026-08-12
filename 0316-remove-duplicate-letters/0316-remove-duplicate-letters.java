8b8742d4016e9eaa828b5984be28d77b33c2f27b
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-12 14:43:39 +0530

    Time: 5 ms (8.69%), Space: 44.8 MB (6.15%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,38 +1,30 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        HashMap<Character,Integer>hm=new HashMap<>();
-        Stack<Character>ss=new Stack<>();
-        HashSet<Character>hs=new HashSet<>();
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(hm.containsKey(c)){
-                hm.put(c,hm.get(c)+1);
-            }else{
-                hm.put(c,1);
-            }
+       
+     HashMap<Character,Integer>hm=new HashMap<>();
+     for(int i=0;i<s.length();i++){
+        char curr=s.charAt(i);
+        hm.put(curr,i);
+     }
+     Stack<Character>ss=new Stack<>();
+     HashSet<Character>hs=new HashSet<>();
+     for(int i=0;i<s.length();i++){
+        char curr=s.charAt(i);
+        if(hs.contains(curr)){
+            continue;
         }
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(hs.contains(c)){
-                int val=hm.get(c)-1;
-                hm.remove(c);
-                hm.put(c,val);
-                continue;
-            }
-            while(!ss.isEmpty() && ss.peek()>c && hm.get(ss.peek())>1){
-                int val=hm.get(ss.peek());
-                hm.remove(ss.peek());
-                hm.put(ss.peek(),val-1);
-                hs.remove(ss.peek());
-                ss.pop();
-            }
-            hs.add(c);
-            ss.push(c);
+        while(!ss.isEmpty() && ss.peek()>curr && hm.get(ss.peek())>i){
+            hs.remove(ss.peek());
+            ss.pop();
         }
-        StringBuilder sb=new StringBuilder();
-        while(!ss.isEmpty()){
-            sb.append(ss.pop());
-        }
-        return sb.reverse().toString();
+        hs.add(curr);
+        ss.push(curr);
+     }
+     StringBuilder sb=new StringBuilder();
+     while(!ss.isEmpty()){
+        sb.append(ss.pop());
+     }
+     return sb.reverse().toString();
+
     }
 }
