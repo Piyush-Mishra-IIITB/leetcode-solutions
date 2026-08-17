29a1281f9b49ddfea1722893bcc70afc59453daf
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-17 20:16:32 +0530

    Time: 7 ms (36.69%), Space: 45.3 MB (53.42%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,27 +1,33 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] arr, int target) {
-
-        Arrays.sort(arr);
-        List<List<Integer>> ans=new ArrayList<>();
-       List<Integer>ll=new ArrayList<>();
-       helper(arr,ans,ll,0,target);
-       return ans;
+      Arrays.sort(arr);
+    List<List<Integer>>ans=new ArrayList<>();
+    List<Integer>ll=new ArrayList<>();
+    helper(0,arr,target,ll,ans,0);
+    return ans;
     }
-    public void helper(int arr[],List<List<Integer>> ans, List<Integer>ll,int ind,int target){
-      if(target==0){
-        ans.add(new ArrayList<>(ll));
-        return;
-      }
-      for(int i=ind;i<arr.length;i++){
-          if(arr[i]>target){
-            break;
+    public void helper(int ind,int arr[],int target,List<Integer>ll,List<List<Integer>>ans,int sum){
+          if(sum==target){
+              ans.add(new ArrayList<>(ll));
+              return;
           }
-          if(i>ind && arr[i]==arr[i-1]){
-            continue;
+            if(ind==arr.length){
+                return;
+            }
+        
+          for(int i=ind;i<arr.length;i++){
+              if(i>ind && arr[i]==arr[i-1]){
+                continue;
+              }
+              if(sum>target){
+                 break;
+              }
+                
+                ll.add(arr[i]);
+                int newSum=sum+arr[i];
+                helper(i+1,arr,target,ll,ans,newSum);
+                ll.remove(ll.size()-1);
+            
           }
-          ll.add(arr[i]);
-          helper(arr,ans,ll,i+1,target-arr[i]);
-          ll.remove(ll.size()-1);
-      }
     }
 }
