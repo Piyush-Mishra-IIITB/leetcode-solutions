31bb426e3932931530d2f2da140fbc4e5479a310
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-20 15:43:06 +0530

    Time: 279 ms (14.37%), Space: 59.1 MB (60.24%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(arr);
-        int n=arr.length;
-        for(int i=0;i<n-1;i++){
+        
+       Arrays.sort(arr);
+        List<List<Integer>>ans=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
             if(i>0 && arr[i]==arr[i-1]){
                 continue;
             }
-            int start=i+1;
-            int end=n-1;
-            int target=-arr[i];
-            while(start<end){
-                if(arr[start]+arr[end]==target){
-                    List<Integer>ll=new ArrayList<>();
-                    ll.add(-(target));
-                    ll.add(arr[start]);
-                    ll.add(arr[end]);
-                    result.add(ll);
-                    while(start>n-1 &&arr[start]==arr[start+1]){
-                        start++;
-                    }while(end>1 && arr[end]==arr[end-1]){
-                        end--;
+            for(int j=i+1;j<arr.length;j++){
+                 if(j>i+1 && arr[j]==arr[j-1]){
+                     continue;
+                }
+                int target=-(arr[i]+arr[j]);
+                int start=j+1;
+                int end=arr.length-1;
+                while(start<=end){
+                    int mid=start+(end-start)/2;
+                    if(arr[mid]==target){
+                      List<Integer>ll=new ArrayList<>();
+                      ll.add((target));
+                      ll.add(arr[i]);
+                      ll.add(arr[j]);
+                      ans.add(new ArrayList<>(ll));
+                      break;
                     }
-                    start++;
-                    end--;
-                }else if(arr[start]+arr[end]<target){
-                    start++;
-                }else{
-                    end--;
+                    else if(arr[mid]>target){
+                         end=mid-1;
+                    }else{
+                        start=mid+1;
+                    }
                 }
             }
         }
-       return result;
+        return ans;
     }
 }
