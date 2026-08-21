44bdf0f8bf1ecb04e3fed5c8e4ba752db8b7408e
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-21 07:18:14 +0530

    Time: 3 ms (3.91%), Space: 47.7 MB (8.74%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -14,47 +14,26 @@
  * }
  */
 class Solution {
-    public class pair{
-        int org;
-        int newOne;
-        public pair(int org,int newOne){
-            this.org=org;
-            this.newOne=newOne;
-        }
-    }
     public TreeNode convertBST(TreeNode root) {
-        List<Integer>in=new ArrayList<>();
-        inorder(in,root);
-        // for(int i=0;i<in.size();i++){
-        //        System.out.println(in.get(i));
-        // }
-        
-        List<Integer>prefixSum=new ArrayList<>();
-        int curr=0;
-        for(int i=0;i<in.size();i++){
-            curr+=in.get(i);
-        }
-        System.out.println("totalSum" + curr);
         HashMap<Integer,Integer>hm=new HashMap<>();
-        int newOneSum=0;
-        for(int i=0;i<in.size();i++){
-            if(i>0){
-                curr=curr-in.get(i-1);
-            }
-            int c=curr;
-            System.out.println(i+"CurrSum" +c);
-            hm.put(in.get(i),c);
+        List<Integer>ll=new ArrayList<>();
+        inorder(ll,root);
+        int sum=0;
+        for(int i=ll.size()-1;i>=0;i--){
+              sum+=ll.get(i);
+              hm.put(ll.get(i),sum);
         }
-        helper(root,hm);
+        dfs(hm,root);
         return root;
     }
-    public void helper(TreeNode root,HashMap<Integer,Integer>hm){
-         if(root==null){
-            return;
-         }
-         root.val=hm.get(root.val);
-         helper(root.left,hm);
-         helper(root.right,hm);
+    public void dfs(HashMap<Integer,Integer>hm,TreeNode root){
+        if(root==null){
+            return ;
+        }
+        int val=hm.get(root.val);
+        root.val=val;
+        dfs(hm,root.left);
+        dfs(hm,root.right);
     }
     public void inorder(List<Integer>ll,TreeNode root){
         if(root==null){
