c9f2774413703d20c0897cab5834aaa2ce50620b
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-21 07:15:09 +0530

    Time: 0 ms (100%), Space: 42.9 MB (57.08%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        List<Integer>ll=new ArrayList<>();
+        inorder(ll,root);
+        int sum=0;
+        for(int i=ll.size()-1;i>=0;i--){
+              sum+=ll.get(i);
+              hm.put(ll.get(i),sum);
+        }
+        dfs(hm,root);
+        return root;
+    }
+    public void dfs(HashMap<Integer,Integer>hm,TreeNode root){
+        if(root==null){
+            return ;
+        }
+        int val=hm.get(root.val);
+        root.val=val;
+        dfs(hm,root.left);
+        dfs(hm,root.right);
+    }
+    public void inorder(List<Integer>ll,TreeNode root){
+        if(root==null){
+            return;
+        }
+        inorder(ll,root.left);
+        ll.add(root.val);
+        inorder(ll,root.right);
+    }
+}
