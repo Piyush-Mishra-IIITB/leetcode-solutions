23f386e125c700ca053c1bef2bc6b8e4ae9dab59
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-22 16:32:03 +0530

    Time: 1400 ms (5.7%), Space: 103.3 MB (93.55%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,34 +1,29 @@
 class Solution {
-
     public class pair{
-
-        int first;
-        int second;
-
-        public pair(int first, int second){
-
-            this.first = first;   
-            this.second = second; 
+        int val;
+        int ind;
+        public pair(int val,int ind){
+            this.val=val;
+            this.ind=ind;
         }
     }
-
-    public int[] dailyTemperatures(int[] arr) {
-        Stack<pair> ss = new Stack<>();
-        int n = arr.length;
-        int ans[] = new int[n];
-        for(int i = n - 1; i >= 0; i--){
-            int curr = arr[i];
-            while(!ss.isEmpty() && ss.peek().first <= curr){
+    public int[] dailyTemperatures(int[] t) {
+        Stack<pair>ss=new Stack<>();
+        int ans[]=new int[t.length];
+        for(int i=t.length-1;i>=0;i--){
+             int curr=t[i];
+            while(!ss.isEmpty() && ss.peek().val<=curr){
+                System.out.println("del" + ss.peek().val + ss.peek().ind);
                 ss.pop();
             }
             if(ss.isEmpty()){
-                ans[i] = 0;
+                ans[i]=0;
+            }else{
+                ans[i]=ss.peek().ind-i;
             }
-            else{
-                ans[i] = ss.peek().second - i;
-            }
-            ss.push(new pair(curr, i));
+            ss.push(new pair(curr,i));
         }
+
         return ans;
     }
 }
