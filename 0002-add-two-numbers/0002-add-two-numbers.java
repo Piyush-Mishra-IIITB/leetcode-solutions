e6e065db65d85747b150cf818949ea6e83590d17
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-24 10:40:00 +0530

    Time: 1 ms (100%), Space: 46.3 MB (77.55%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,40 +1,71 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-      ListNode temp1=l1;
-      ListNode temp2=l2;
-      int carry=0;
-      ListNode temp3=new ListNode(-1);
-      ListNode temp4=temp3;
-      while(temp1!=null && temp2!=null){
-        int a=temp1.val;
-        int b=temp2.val;
-        int c=(a+b+carry)%10;
-        temp4.next=new ListNode(c);
-        temp4=temp4.next;
-        carry=(a+b+carry)/10;
-        temp1=temp1.next;
-        temp2=temp2.next;
-      }
-    while(temp1!=null){
-        int a=temp1.val;
-        
-        int c=(a+carry)%10;
-        temp4.next=new ListNode(c);
-        temp4=temp4.next;
-        carry=(a+carry)/10;
-        temp1=temp1.next;
+        ListNode temp1=l1;
+        ListNode temp2=l2;
+        int carry=0;
+        ListNode op=new ListNode(-1);
+        ListNode var=op;
+        while(temp1!=null && temp2!=null){
+            if(carry==0){
+                int val1=temp1.val+temp2.val;
+                carry=val1/10;
+                ListNode newOne=new ListNode(val1%10);
+                var.next=newOne;
+                var=var.next;
+                temp1=temp1.next;
+                temp2=temp2.next;
+            }else{
+                int val1=temp1.val+temp2.val+carry;
+                carry=val1/10;
+                ListNode newOne=new ListNode(val1%10);
+                var.next=newOne;
+                var=var.next;
+                temp1=temp1.next;
+                temp2=temp2.next;
+            }
+        }
+        while(temp1!=null){
+             int val1=temp1.val+carry;
+                carry=val1/10;
+                ListNode newOne=new ListNode(val1%10);
+                var.next=newOne;
+                var=var.next;
+                temp1=temp1.next;
+        }
+        while(temp2!=null){
+             int val1=temp2.val+carry;
+                carry=val1/10;
+                ListNode newOne=new ListNode(val1%10);
+                var.next=newOne;
+                var=var.next;
+                temp2=temp2.next;
+        }
+        if(carry!=0){
+            ListNode c= new ListNode(carry);
+            var.next=c;
+            var=var.next;
+        }
+        return op.next;
     }
-    while(temp2!=null){
-        int b=temp2.val;
-        int c=(b+carry)%10;
-        temp4.next=new ListNode(c);
-        temp4=temp4.next;
-        carry=(b+carry)/10;
-        temp2=temp2.next;
+    public ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
     }
-    if(carry!=0){
-        temp4.next=new ListNode(carry);
-    }
-    return temp3.next;
-}}
+}
