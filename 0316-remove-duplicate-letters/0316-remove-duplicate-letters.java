929127e8b9a0005cc7d4e912b0f71fe5e81a8c1b
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-24 18:13:24 +0530

    Time: 4 ms (17.38%), Space: 44.7 MB (7.24%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,30 +1,33 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-       
-     HashMap<Character,Integer>hm=new HashMap<>();
-     for(int i=0;i<s.length();i++){
-        char curr=s.charAt(i);
-        hm.put(curr,i);
-     }
-     Stack<Character>ss=new Stack<>();
-     HashSet<Character>hs=new HashSet<>();
-     for(int i=0;i<s.length();i++){
-        char curr=s.charAt(i);
-        if(hs.contains(curr)){
-            continue;
+        HashMap<Character,Integer>hm=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char curr=s.charAt(i);
+            hm.put(curr,i);
         }
-        while(!ss.isEmpty() && ss.peek()>curr && hm.get(ss.peek())>i){
-            hs.remove(ss.peek());
-            ss.pop();
-        }
-        hs.add(curr);
-        ss.push(curr);
-     }
-     StringBuilder sb=new StringBuilder();
-     while(!ss.isEmpty()){
-        sb.append(ss.pop());
-     }
-     return sb.reverse().toString();
 
+        Stack<Character>ss=new Stack<>();
+        HashSet<Character>hs=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            char curr=s.charAt(i);
+             if(hs.contains(curr)){
+                continue;
+            }
+            while(!ss.isEmpty() && curr<ss.peek() && hm.get(ss.peek())>i){
+                char c=ss.peek();
+                if(hs.contains(c)){
+                    hs.remove(c);
+                }
+                ss.pop();
+            }
+            hs.add(curr);
+            ss.push(curr);
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!ss.isEmpty()){
+            sb.append(ss.pop());
+        }
+        return sb.reverse().toString();
+    
     }
 }
