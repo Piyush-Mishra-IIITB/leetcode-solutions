a923290c8a9ca1d930b8d8253685eabd44c886d4
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-24 18:12:53 +0530

    Time: 5 ms (6.74%), Space: 42.9 MB (42.63%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -2,32 +2,26 @@ class Solution {
     public String smallestSubsequence(String s) {
         HashMap<Character,Integer>hm=new HashMap<>();
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(hm.containsKey(c)){
-                hm.put(c,hm.get(c)+1);
-            }else{
-                hm.put(c,1);
-            }
+            char curr=s.charAt(i);
+            hm.put(curr,i);
         }
-        HashSet<Character>hs=new HashSet<>();
+
         Stack<Character>ss=new Stack<>();
+        HashSet<Character>hs=new HashSet<>();
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-              if(hs.contains(c)){
-                int val=hm.get(c)-1;
-                hm.remove(c);
-                hm.put(c,val);
+            char curr=s.charAt(i);
+             if(hs.contains(curr)){
                 continue;
-               }
-             while(!ss.isEmpty() && ss.peek()>c && hm.get(ss.peek())>1){
-                int val=hm.get(ss.peek());
-                hm.remove(ss.peek());
-                hm.put(ss.peek(),val-1);
-                hs.remove(ss.peek());
+            }
+            while(!ss.isEmpty() && curr<ss.peek() && hm.get(ss.peek())>i){
+                char c=ss.peek();
+                if(hs.contains(c)){
+                    hs.remove(c);
+                }
                 ss.pop();
-             }
-            ss.push(c);
-            hs.add(c);
+            }
+            hs.add(curr);
+            ss.push(curr);
         }
         StringBuilder sb=new StringBuilder();
         while(!ss.isEmpty()){
