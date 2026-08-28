dd0e03c068d427cbefc45dbed3c5f29846cc4a05
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-28 12:49:56 +0530

    Time: 776 ms (5.01%), Space: 280.8 MB (14.5%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,29 +1,40 @@
 class Solution {
+    public class pair{
+        int val;
+        int val2;
+        public pair(int val,int val2){
+            this.val=val;
+            this.val2=val2;
+        }
+    }
     public int threeSumClosest(int[] arr, int target) {
-
         Arrays.sort(arr);
-
-        int close = arr[0] + arr[1] + arr[2];
-
-        for (int i = 0; i < arr.length - 2; i++) {
-
-            int start = i + 1;
-            int end = arr.length - 1;
-            while (start < end) {
-                int sum = arr[i] + arr[start] + arr[end];
-                if (Math.abs(sum - target) < Math.abs(close - target)) {
-                    close = sum;
+        PriorityQueue<pair>pq=new PriorityQueue<>((a,b)->a.val-b.val);
+        for(int i=0;i<arr.length;i++){
+            if(i>0 && arr[i]==arr[i-1] ){
+                continue;
+            }
+            for(int j=i+1;j<arr.length;j++){
+                if(j>i+1 && arr[j]==arr[j-1] ){
+                continue;
                 }
-                if (sum == target) {
-                    return target;
-                } else if (sum > target) {
-                    end--;
-                } else {
-                    start++;
+                int start=j+1;
+                int end=arr.length-1;
+                while(start<=end){
+                     int mid=start+(end-start)/2;
+                    //  System.out.println(target-(arr[i]+arr[mid]+arr[j]));
+                    //  System.out.println("next");
+                    //  System.out.println(target-(arr[i]+arr[mid]+arr[j])+"next"+(arr[i]+arr[mid]+arr[j]));
+                     pq.add(new pair(Math.abs(target-(arr[i]+arr[mid]+arr[j])),(arr[i]+arr[mid]+arr[j])));
+                     if(arr[i]+arr[mid]+arr[j]>=target){
+                        end=mid-1;
+                     }else{
+                        start=mid+1;
+                     }
                 }
+               
             }
         }
-
-        return close;
+        return pq.peek().val2;
     }
 }
