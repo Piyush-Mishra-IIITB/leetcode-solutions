fde41b8653b729e3e9d3a456a1e65cff1d3b7712
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-28 13:45:36 +0530

    Time: 0 ms (100%), Space: 44.4 MB (80.18%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,48 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String>s=new ArrayList<>();
-        String ss="";
-        helper(ss,s,n,0,0);
-        List<String>ans=new ArrayList<>();
-        for(int i=0;i<s.size();i++){
-            if(verify(s.get(i))){
-                ans.add(s.get(i));
-            }
-        }
-        return ans;
-        
-        }
-        public boolean verify(String s){
-
-
-
-        Stack<Character>cc=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c=='('){
-                cc.push(c);
-            }else{
-                if(cc.isEmpty()){
-                    return false;
-                }
-                else{
-                    cc.pop();
-                }
-            }
-        }
-        return cc.isEmpty();
-        }
-   public void helper(String ss,List<String>s,int n,int open,int close){
-    if(ss.length()==2*n){
-        s.add(ss);
-        return;
+        StringBuilder sb=new StringBuilder();
+        List<String>ll=new ArrayList<>();
+        helper(n,0,0,ll,sb);
+        return ll;
     }
-    if(open<n){
-     helper(ss+'(',s,n,open+1,close);
+    public void helper(int n,int open,int close,List<String>ll,StringBuilder sb){
+        if(sb.length()==2*n){
+            String toAdded=sb.toString();
+            ll.add(toAdded);
+            return;
+        }
+        if(open<n){
+            int init=sb.length();
+            sb.append('(');
+            helper(n,open+1,close,ll,sb);
+            sb.setLength(init);
+        }if(open>close){
+             int init=sb.length();
+            sb.append(')');
+            helper(n,open,close+1,ll,sb);
+            sb.setLength(init);
+        }
     }
-    if(close<n){
-     helper(ss+')',s,n,open,close+1);
-    }
-   
-   }
 }
