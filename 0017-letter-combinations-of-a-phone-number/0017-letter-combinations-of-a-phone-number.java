9a3353ad5ac1849b3143c36bc1da59ff5bc81943
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-28 13:15:56 +0530

    Time: 0 ms (100%), Space: 43.6 MB (52.28%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,33 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+        HashMap<Integer,String>hm=new HashMap<>();
+        hm.put(2,"abc");
+        hm.put(3,"def");
+        hm.put(4,"ghi");
+        hm.put(5,"jkl");
+        hm.put(6,"mno");
+        hm.put(7,"pqrs");
+        hm.put(8,"tuv");
+        hm.put(9,"wxyz");
+        StringBuilder sb=new StringBuilder();
+        List<String>ll=new ArrayList<>();
+        helper(0,digits,sb,ll,hm);
+        return ll;
 
-        if(digits.length() == 0)
-            return new ArrayList<>();
-
-        HashMap<Character,String> hm = new HashMap<>();
-
-        hm.put('2',"abc");
-        hm.put('3',"def");
-        hm.put('4',"ghi");
-        hm.put('5',"jkl");
-        hm.put('6',"mno");
-        hm.put('7',"pqrs");
-        hm.put('8',"tuv");
-        hm.put('9',"wxyz");
-
-        List<String> ans = new ArrayList<>();
-        helper(0, hm, new StringBuilder(), digits, ans);
-        return ans;
     }
-
-    public void helper(int ind, HashMap<Character,String> hm,StringBuilder sb, String ss, List<String> ans){
-
-        if(ind == ss.length()){
-            ans.add(sb.toString());
+    public void helper(int ind,String digit,StringBuilder sb,List<String>ll,HashMap<Integer,String>hm){
+        if(sb.length()==digit.length()){
+            String toAdded=sb.toString();
+            ll.add(toAdded);
             return;
         }
-        String letters = hm.get(ss.charAt(ind));
-
-        for(int i = 0; i < letters.length(); i++){
-            sb.append(letters.charAt(i));
-            helper(ind + 1, hm, sb, ss, ans);
-            sb.deleteCharAt(sb.length() - 1);
+        char curr=digit.charAt(ind);
+        String now=hm.get(curr-'0');
+        for(int i=0;i<now.length();i++){
+          int init=sb.length();
+           sb.append(now.charAt(i));
+           helper(ind+1,digit,sb,ll,hm);
+           sb.setLength(init);
         }
     }
 }
