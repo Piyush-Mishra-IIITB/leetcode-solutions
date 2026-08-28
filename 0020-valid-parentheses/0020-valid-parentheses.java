46750b860f0e3db09bf7ecc882601fe50c754d60
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-08-28 13:25:12 +0530

    Time: 3 ms (86.07%), Space: 43.1 MB (73.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,53 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>ss=new Stack<>();
-        int length=s.length();
-        int i=0;
-        int open=0;
-        int close=0;
-        while(i<length){
-            char c=s.charAt(i);
-            if(c=='(' || c=='{' || c =='['){
-                ss.push(c);
-                open++;
-            }else if(c==')'){
-                while(!ss.isEmpty() && ss.peek()!='('){
-                    ss.pop();
-                }
-                if(ss.isEmpty()){
-                    return false;
-                }
-                if(!ss.isEmpty()){
-                    ss.pop();}
-                    close++;
-               
-            }else if(c=='}'){
-                while(!ss.isEmpty() && ss.peek()!='{'){
-                    ss.pop();
-                }
-                if(ss.isEmpty()){
-                    return false;
-                }
-                if(!ss.isEmpty()){
-                    ss.pop();}
-                    close++;
-            }else if(c==']'){
-                while(!ss.isEmpty() && ss.peek()!='['){
-                    ss.pop();
-                }
-                if(ss.isEmpty()){
-                    return false;
-                }
-                if(!ss.isEmpty()){
-                    ss.pop();}
-                    close++;
-            }
-            i++;
-
-        }
-        if(open==close){
-             return ss.isEmpty();
-        }
-        return false;
-}
+      Stack<Character>ss=new Stack<>();
+      for(int i=0;i<s.length();i++){
+        char curr=s.charAt(i);
+         if(curr=='(' || curr=='{' || curr=='['){
+            ss.push(curr);
+         }else if(curr==')'){
+             if(!ss.isEmpty() && ss.peek()=='('){
+                ss.pop();
+             }else{
+                return false;
+             }
+         }else if(curr=='}'){
+             if(!ss.isEmpty() && ss.peek()=='{'){
+                ss.pop();
+             }else{
+                return false;
+             }
+         }else{
+            if(!ss.isEmpty() && ss.peek()=='['){
+                ss.pop();
+             }else{
+                return false;
+             }
+         }
+      }
+      return ss.isEmpty() ? true:false;
+    }
 }
