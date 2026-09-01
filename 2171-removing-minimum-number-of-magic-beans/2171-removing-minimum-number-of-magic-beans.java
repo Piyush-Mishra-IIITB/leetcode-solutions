b8be55106c3280341f833bbbe7ffaf30c7b7c15d
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-01 14:19:28 +0530

    Time: 55 ms (9.7%), Space: 118.2 MB (7.17%) - LeetHub

--- 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.java
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.java
@@ -1,17 +1,31 @@
 class Solution {
-    public long minimumRemoval(int[] beans) {
-        Arrays.sort(beans);
-        long ans=Long.MAX_VALUE;
-        long total=0;
-        for(int i=0;i<beans.length;i++){
-              total+=beans[i];
+    public long minimumRemoval(int[] arr) {
+
+        Arrays.sort(arr);
+
+        List<Long> ll = new ArrayList<>();
+
+        ll.add((long) arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            ll.add(ll.get(ll.size() - 1) + (long) arr[i]);
         }
-       for(int i=0;i<beans.length;i++){
-          ans=Math.min(ans,helper(beans,i,total));
-       }
-        return ans;
-    }
-    public long helper(int arr[],int ind,long total){
-         return total-(long)arr[ind]*(arr.length-ind);
+
+        long op = Long.MAX_VALUE;
+
+        for (int i = 0; i < ll.size(); i++) {
+
+            long toMake0 = (i == 0) ? 0 : ll.get(i - 1);
+
+            long toEqual = ll.get(ll.size() - 1) - toMake0;
+
+            long finals = (long) arr[i] * (ll.size() - i);
+
+            long val = toEqual - finals;
+
+            op = Math.min(op, toMake0 + val);
+        }
+
+        return op;
     }
 }
