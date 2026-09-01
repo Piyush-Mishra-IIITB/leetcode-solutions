299e211f03b416220bd8bb83c22c281ac8558745
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-01 12:00:13 +0530

    Time: 69 ms (25%), Space: 139.6 MB (13.97%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,30 +1,28 @@
 class Solution {
-    int ans=0;
-    public int minReorder(int n, int[][] conn) {
-        List<List<int []>>adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
+    Map<Integer, List<int[]>> graph = new HashMap<>();
+    Set<Integer> set = new HashSet<>();
+    int changed = 0;
+    public int minReorder(int n, int[][] connections) {
+        for(int i=0; i<n; i++){
+            graph.put(i, new ArrayList<int[]>());
         }
-        for(int i=0;i<conn.length;i++){
-            int a[]=conn[i];
-            adj.get(a[0]).add(new int[]{a[1],1});
-            adj.get(a[1]).add(new int[]{a[0],0});
+        for(int[] conn: connections){
+            graph.get(conn[0]).add(new int[]{conn[1], 0});
+            graph.get(conn[1]).add(new int[]{conn[0], 1});
         }
-        boolean vis[]=new boolean[n];
-        helper(0,adj,vis);
-        return ans;
+        dfs(0);
+        return changed;
     }
-    public void helper(int i,List<List<int []>>adj,boolean vis[]){
-        vis[i]=true;
-        for(int []it:adj.get(i)){
-             int a=it[0];
-             int b=it[1];
-             if(vis[a]==false){
-                if(b==1){
-                    ans+=1;
-                }
-                helper(a,adj,vis);
-             }
+    void dfs(int x){
+        set.add(x);
+        for(int[] conn: graph.get(x)){
+            if(set.contains(conn[0])) continue;
+            if(conn[1] == 0){
+                changed++;
+                dfs(conn[0]);
+            } else {
+                dfs(conn[0]);
+            }
         }
     }
 }
