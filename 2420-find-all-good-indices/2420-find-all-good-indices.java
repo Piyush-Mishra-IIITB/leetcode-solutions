1c5847d4714674862add9feb1c0679360240b8b0
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-01 16:27:20 +0530

    Time: 7 ms (32.8%), Space: 99.2 MB (59.79%) - LeetHub

---
+++ 2420-find-all-good-indices/2420-find-all-good-indices.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> goodIndices(int[] arr, int k) {
+        int n = arr.length;
+
+        int[] small = new int[n];
+        int[] great = new int[n];
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] <= arr[i - 1]) {
+                small[i] = small[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (arr[i] <= arr[i + 1]) {
+                great[i] = great[i + 1] + 1;
+            }
+        }
+
+        List<Integer> ll = new ArrayList<>();
+
+        for (int i = k; i < n - k; i++) {
+
+            if (small[i-1] >= k-1 && great[i+1] >= k-1) {
+                ll.add(i);
+            }
+        }
+
+        return ll;
+    }
+}
