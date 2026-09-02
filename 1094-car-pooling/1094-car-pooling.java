19ebd977eeb0b4932a4f8561a4d5eef5837c7450
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-02 18:13:01 +0530

    Time: 119 ms (5.18%), Space: 47.2 MB (11.15%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,39 +1,37 @@
 class Solution {
-    public class pair{
-        int no;
-        int val;
-        public pair(int no,int val){
-            this.no=no;
-            this.val=val;
-        }
-    }
-    public boolean carPooling(int[][] trips, int capacity) {
-        List<pair>ll=new ArrayList<>();
-        for(int i=0;i<trips.length;i++){
-            int a[]=trips[i];
-            int u=a[0];
-            int v=a[1];
-            int x=a[2];
-            ll.add(new pair(v,u));
-            ll.add(new pair(x,-u));
-        }
-        Collections.sort(ll,(a,b)->{
-            if(a.no!=b.no){
-                return a.no-b.no;
-            }
-            return a.val-b.val;
+    public boolean carPooling(int[][] arr, int k) {
+        Arrays.sort(arr,(a,b)->{
+           if(a!=b){
+            return a[0]-b[0];
+           }
+           return a[1]-b[1];
         });
-        int sum=0;
-        for(int i=0;i<ll.size();i++){
-            pair p=ll.get(i);
-            System.out.println("a"+ p.no + "b" +p.val);
-        }
-        for(int i=0;i<ll.size();i++){
-            pair p=ll.get(i);
-            sum+=p.val;
-            if(sum>capacity){
-                return false;
-            }
+        
+        // for(int i=0;i<arr.length;i++){
+        //    for(int j=0;j<3;j++){
+        //     System.out.println(arr[i][j]);
+        //    }
+        // }
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+               int a[]=arr[i];
+               int u=a[0];
+               int v=a[1];
+               int x=a[2];
+               for(int j=v;j<x;j++){
+                  if(hm.containsKey(j)){
+                    int curr=hm.get(j);
+                    if(curr+u>k){
+                        return false;
+                    }
+                    hm.put(j,curr+u);
+                  }else{
+                    if(u>k){
+                        return false;
+                    }
+                    hm.put(j,u);
+                  }
+               }
         }
         return true;
     }
