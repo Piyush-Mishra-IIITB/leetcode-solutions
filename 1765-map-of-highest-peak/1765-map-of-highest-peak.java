01b0b6b5a8ed24e1c0f98fbb85c7f24e4e645d08
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-12 16:58:00 +0530

    Time: 34 ms (96.98%), Space: 184.1 MB (47.4%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.java
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -1,55 +1,62 @@
 class Solution {
-    public class pair{
+    public class pair {
         int row;
         int col;
-        int dist;
-        public pair(int row,int col,int dist){
-            this.row=row;
-            this.col=col;
-            this.dist=dist;
+        int val;
+
+        public pair(int row, int col, int val) {
+            this.row = row;
+            this.col = col;
+            this.val = val;
         }
     }
+
     public int[][] highestPeak(int[][] arr) {
-        int n=arr.length;
-        int m=arr[0].length;
-        int d[][]=new int[n][m];
-        boolean vis[][]=new boolean[n][m];
-        Queue<pair>qq=new LinkedList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]==1){
-                    vis[i][j]=true;
-                    d[i][j]=0;
-                    qq.add(new pair(i,j,0));
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int[][] ans = new int[n][m];
+        Queue<pair> qq = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (arr[i][j] == 1) {
+                    qq.add(new pair(i, j, 0));
+                    ans[i][j] = 0;
+                } else {
+                    ans[i][j] = -1;  // unvisited
                 }
             }
         }
 
-        while(!qq.isEmpty()){
-            pair curr=qq.poll();
-            int row=curr.row;
-            int col=curr.col;
-            int dist=curr.dist;
-            if(row+1<n && vis[row+1][col]==false){
-                vis[row+1][col]=true;
-                qq.add(new pair(row+1,col,dist+1));
-                d[row+1][col]=dist+1;
+        while (!qq.isEmpty()) {
+            pair p = qq.poll();
+
+            int r = p.row;
+            int c = p.col;
+
+            if (r + 1 < n && ans[r + 1][c] == -1) {
+                ans[r + 1][c] = p.val + 1;
+                qq.add(new pair(r + 1, c, p.val + 1));
             }
-            if(row-1>=0 && vis[row-1][col]==false){
-                vis[row-1][col]=true;
-                qq.add(new pair(row-1,col,dist+1));
-                d[row-1][col]=dist+1;
+
+            if (r - 1 >= 0 && ans[r - 1][c] == -1) {
+                ans[r - 1][c] = p.val + 1;
+                qq.add(new pair(r - 1, c, p.val + 1));
             }
-            if(col+1<m && vis[row][col+1]==false){
-                vis[row][col+1]=true;
-                qq.add(new pair(row,col+1,dist+1));
-                d[row][col+1]=dist+1;
-            }if(col-1>=0 && vis[row][col-1]==false){
-                vis[row][col-1]=true;
-                qq.add(new pair(row,col-1,dist+1));
-                d[row][col-1]=dist+1;
+
+            if (c + 1 < m && ans[r][c + 1] == -1) {
+                ans[r][c + 1] = p.val + 1;
+                qq.add(new pair(r, c + 1, p.val + 1));
+            }
+
+            if (c - 1 >= 0 && ans[r][c - 1] == -1) {
+                ans[r][c - 1] = p.val + 1;
+                qq.add(new pair(r, c - 1, p.val + 1));
             }
         }
-        return d;
+
+        return ans;
     }
 }
