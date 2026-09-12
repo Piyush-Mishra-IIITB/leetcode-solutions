542c744c0fcbc33f187e45082d4743b650047a00
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-12 16:55:54 +0530

    Time: 15 ms (43.27%), Space: 49.5 MB (22.31%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,64 +1,62 @@
 class Solution {
-    public class pair{
+    public class pair {
         int row;
         int col;
-        int dis;
-        public pair(int row,int col,int dis){
-            this.row=row;
-            this.col=col;
-            this.dis=dis;
+        int val;
+
+        public pair(int row, int col, int val) {
+            this.row = row;
+            this.col = col;
+            this.val = val;
         }
     }
+
     public int[][] updateMatrix(int[][] arr) {
-        Queue<pair>qq=new LinkedList<>();
-        int n=arr.length;
-        int m=arr[0].length;
-        int dist[][]=new int [n][m];
-        boolean vis[][]=new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]==0){
-                    qq.add(new pair(i,j,0));
-                    vis[i][j]=true;
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int[][] ans = new int[n][m];
+        Queue<pair> qq = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (arr[i][j] == 0) {
+                    qq.add(new pair(i, j, 0));
+                    ans[i][j] = 0;
+                } else {
+                    ans[i][j] = -1;  
                 }
             }
         }
-        while(!qq.isEmpty()){
-            pair p=qq.poll();
-            int r=p.row;
-            int c=p.col;
-            int d=p.dis;
-            if(r+1<n){
-                if(vis[r+1][c]==false){
-                    vis[r+1][c]=true;
-                    dist[r+1][c]=d+1;
-                    qq.add(new pair(r+1,c,d+1));
-                }
-            }
-            if(r-1>=0){
-                if(vis[r-1][c]==false){
-                    vis[r-1][c]=true;
-                    dist[r-1][c]=d+1;
-                    qq.add(new pair(r-1,c,d+1));
-                }
-            }
-            if(c+1<m){
-                if(vis[r][c+1]==false){
-                    vis[r][c+1]=true;
-                    dist[r][c+1]=d+1;
-                    qq.add(new pair(r,c+1,d+1));
-                }
-            }
-            if(c-1>=0){
-                if(vis[r][c-1]==false){
-                    vis[r][c-1]=true;
-                    dist[r][c-1]=d+1;
-                    qq.add(new pair(r,c-1,d+1));
-                }
+
+        while (!qq.isEmpty()) {
+            pair p = qq.poll();
+
+            int r = p.row;
+            int c = p.col;
+
+            if (r + 1 < n && ans[r + 1][c] == -1) {
+                ans[r + 1][c] = p.val + 1;
+                qq.add(new pair(r + 1, c, p.val + 1));
             }
 
+            if (r - 1 >= 0 && ans[r - 1][c] == -1) {
+                ans[r - 1][c] = p.val + 1;
+                qq.add(new pair(r - 1, c, p.val + 1));
+            }
+
+            if (c + 1 < m && ans[r][c + 1] == -1) {
+                ans[r][c + 1] = p.val + 1;
+                qq.add(new pair(r, c + 1, p.val + 1));
+            }
+
+            if (c - 1 >= 0 && ans[r][c - 1] == -1) {
+                ans[r][c - 1] = p.val + 1;
+                qq.add(new pair(r, c - 1, p.val + 1));
+            }
         }
-        return dist;
+
+        return ans;
     }
-    
 }
