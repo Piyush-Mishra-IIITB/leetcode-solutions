5da1b782e2b542601e2d7ef1498f52710c5f29ea
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-16 18:14:46 +0530

    Time: 960 ms (5.16%), Space: 48.2 MB (77.42%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,17 +1,28 @@
 class Solution {
-    static final int MOD = 1_000_000_007;
+    int k;
+    int mod = 1_000_000_007;
+    int n;
+    int dp[][];
     public int numberOfSets(int n, int k) {
-        long[][] dp = new long[k+1][n];
-        for(int i=0; i<n; i++){
-            dp[0][i] = 1;
+        int res = 0;
+        this.k = k;
+        this.n = n;
+        dp = new int[n+1][k+1];
+        return recurse(0, k);        
+    }
+    int recurse(int start, int currk) {
+        if (currk == 0) return 1;
+        if(dp[start][currk] > 0) return dp[start][currk];
+        int ans = 0;
+        int prev = 0;
+        for (int i=start+1; i<=n-currk; i++){
+            int w = i - start;
+            int total = w * (w + 1) / 2;
+            int current = total - prev;
+            prev = total;
+            int rhs = recurse(i, currk - 1);
+            ans = (int)((ans + (long)rhs * current) % mod);
         }
-        for(int j=1; j<=k; j++){
-            long sum = 0;
-            for(int i=1; i<n; i++){
-                sum = (sum + dp[j-1][i-1]) % MOD;
-                dp[j][i] = (dp[j][i-1] + sum) % MOD;
-            }
-        }
-        return (int) dp[k][n-1];
+        return dp[start][currk] = ans;
     }
 }
