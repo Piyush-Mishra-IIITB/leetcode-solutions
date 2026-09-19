019ea241d6a32a5405656be2733044fbe35aa2ea
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-19 20:09:40 +0530

    Time: 2 ms (71.71%), Space: 47.4 MB (15.74%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,23 +1,18 @@
 class Solution {
     public int maxProduct(int[] arr) {
-        int min_end=arr[0];
-        int var_min=arr[0];
-        int max_end=arr[0];
-        int var_max=arr[0];
-        int product=arr[0];
-
+        int maxr=arr[0];
+        int minr=arr[0];
+        int min=arr[0];
+        int max=arr[0];
         for(int i=1;i<arr.length;i++){
-            int v1=arr[i];
-            int v2=var_max*arr[i];
-             int v3=var_min*arr[i];
-            var_max=Math.max(v3,Math.max(v2,v1));
-            max_end=Math.max(var_max,max_end);
-           
-            var_min=Math.min(v2,Math.min(v1,v3));
-            min_end=Math.min(var_min,min_end);
-            product=Math.max(max_end,min_end);
+            int curr=arr[i];
+            int v1=curr*maxr;
+            int v2=curr*minr;
+            maxr = Math.max(curr, Math.max(v1, v2));
+            minr = Math.min(curr, Math.min(v1, v2));
+            min=Math.min(min,minr);
+            max=Math.max(max,maxr);
         }
-
-        return product;
+        return Math.max(min,max);
     }
 }
