172e4f9df836cfb5632f9e403df240ec4872d4a5
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-21 21:49:54 +0530

    Time: 1 ms (87.92%), Space: 45.4 MB (62.95%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,44 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>ll=new ArrayList<>();
-        boolean vis[]=new boolean[nums.length];
-        helper(nums,ans,ll,vis);
-        return ans;
 
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>(), ans);
+
+        return ans;
     }
-    public void helper(int arr[],List<List<Integer>>ans,List<Integer>ll,boolean vis[]){
-        if(ll.size()==arr.length){
-            ans.add(new ArrayList<>(ll));
+
+    private void backtrack(
+        int[] nums,
+        boolean[] used,
+        List<Integer> curr,
+        List<List<Integer>> ans
+    ) {
+
+        // permutation is complete
+        if (curr.size() == nums.length) {
+            ans.add(new ArrayList<>(curr));
             return;
         }
 
-        for(int i=0;i<arr.length;i++){
-            if(vis[i]==false){
-                ll.add(arr[i]);
-                vis[i]=true;
-                helper(arr,ans,ll,vis);
-                ll.remove(ll.size()-1);
-                vis[i]=false;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i]) {
+                continue;
+            }
+
+            // choose
+            used[i] = true;
+            curr.add(nums[i]);
+
+            // explore
+            backtrack(nums, used, curr, ans);
+
+            // undo choice
+            curr.remove(curr.size() - 1);
+            used[i] = false;
         }
     }
-}}
+}
