32377636eba6334658258476db4eb2a274d2ac58
Piyush-Mishra <piyushmishra8982@gmail.com>
2026-09-25 18:32:03 +0530

    Time: 5 ms (64.06%), Space: 46.7 MB (88.98%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,30 +1,44 @@
- class Solution {
-    public int[] asteroidCollision(int[] arr) {
-        Stack<Integer> ss = new Stack<>();
-       for(int i=0;i<arr.length;i++){
-        int curr=arr[i];
-        if(curr>0){
-            ss.push(curr);
-        }else{
-            while(!ss.isEmpty()&& ss.peek()>0&& ss.peek()<Math.abs(curr)){
-                ss.pop();  
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int asteroid : asteroids) {
+
+            boolean destroyed = false;
+
+            while (!st.isEmpty() && st.peek() > 0 && asteroid < 0) {
+
+                if (st.peek() < -asteroid) {
+                    // Stack asteroid explodes
+                    st.pop();
+                }
+                else if (st.peek() == -asteroid) {
+                    // Both explode
+                    st.pop();
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    // Current asteroid explodes
+                    destroyed = true;
+                    break;
+                }
             }
-            if(!ss.isEmpty()&&ss.peek()==Math.abs(curr)){
-                ss.pop();
-                continue;
-            }
-            if(ss.isEmpty() || ss.peek()<0){
-                ss.push(curr);
-            }
-            else if(ss.peek()>Math.abs(curr)){
-                continue;
+
+            if (!destroyed) {
+                st.push(asteroid);
             }
         }
-       }
-       int ans[]=new int[ss.size()];
-       for(int i=0;i<ss.size();i++){
-        ans[i]=ss.get(i);
-       }
-       return ans;
+
+        int[] ans = new int[st.size()];
+
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = st.get(i);
+        }
+
+        return ans;
     }
 }
